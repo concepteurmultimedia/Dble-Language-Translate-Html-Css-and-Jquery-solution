c0b65344ba3ca4a26348c4e9f1dc6ae28d65f7bc
patrice.salamone <54757025+concepteurmultimedia@users.noreply.github.com>
2024-09-03 17:44:33 +0200

    Add files via upload

---
+++ language.js
@@ -0,0 +1,12 @@
+	function langFrench(){
+		$( "#french" ).on( "click", function() {
+ 			$("#language").attr("href", "./css/Fr.css");
+/* 			alert("french");*/
+		} );
+		}
+	function langEnglish() {
+		$( "#english" ).on( "click", function() {
+ 			$("#language").attr("href", "./css/En.css"); 			
+/* 			alert("english");*/
+		} );
+	}
